58a9064e880fb024eb73ff060f64973fa12d1c7e
Ally Delgado <61644740+aedelgado19@users.noreply.github.com>
2021-04-07 22:37:59 -0700

    finalized

--- main.c
+++ main.c
@@ -1,37 +1,33 @@
+/* Author: Allison Delgado
+   Class: CS201 Spring 2021
+   This program is a typing game that tests the user's
+   typing speed. They are to type out the sentence,
+   "The quick brown fox jumps over the lazy dog"
+   (in a randomized word order), then the computer
+   prints out how long it took them.
+
+   Last updated: April 7, 2021
+ */
+
+
 #include <stdio.h>
+#include <sys/time.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
 #include <stdbool.h>
 
-#define SIZE 9
+#define SIZE 9 //there are 9 words in the array
 
 //function prototypes
-void setUp();
-void gamePlay(char* randomizedArray[SIZE]);
-void swap(int *i, int *j);
+void setUp(char* arr[SIZE]);
+void gamePlay(char* arr[SIZE]);
 
-int main(){
+//main prompts the user to begin and calls setUp() function
+int main(void){
   char input[64];
-  printf("Welcome to Speed Typing.\n");
-  printf("Type 'ready' to begin!\n");
-  scanf("%s", input);
-  
-  if(strncmp(input, "ready", strlen(input)) == 0){
-    setUp();
-  }
-  return 0;
-}
-
-void setUp(){
-  bool used = true; //ensures that no word is used twice
-  int randIndex = 0;
-  const char *randomizedArray[SIZE]; //holds randomized output
-  int usedIndices[SIZE]; //holds already used indices
-  srand(time(NULL));
   
   //fill array with words
-  const char *arr[SIZE];
+  char *arr[SIZE];
   arr[0] = "the";
   arr[1] = "quick";
   arr[2] = "brown";
@@ -42,33 +38,64 @@ void setUp(){
   arr[7] = "lazy";
   arr[8] = "dog";
 
-  //select a random number for the index
-  for(int i = 8; i > 0; i--){
-    randIndex = rand()%i+1;
-    swap(&i, &randIndex);
-    strncpy(randomizedArray[i], arr[randIndex], SIZE);
-    printf("randomizedArray[i] = %s\n", randomizedArray[i]);
+  //print welcome messages and prompt user to begin game
+  printf("Welcome to Speed Typing.\n");
+  printf("Type 'ready' to begin!\n");
+  scanf("%s", input);
+  
+  if(strncmp(input, "ready", strlen(input)) == 0){
+    setUp(arr);
   }
-
-  gamePlay(randomizedArray);
+  return 0;
 }
 
-void swap(int *i, int *j){
-  int temp = 0;
-  temp = *i;
-  *i = *j;
-  *j = temp;
+/* the setUp function uses Fisher-Yates shuffle to
+   randomize the array, then sends this new array to gamePlay();
+ */
+void setUp(char* arr[SIZE]){
+  int randIndex = 0;
+
+  //use srand with the seed as get time of day
+  struct timeval time;
+  gettimeofday(&time, NULL); 
+  srand((time.tv_sec * 1000) + (time.tv_sec / 1000));
+
+  //select a random number for the index
+  //uses Fisher-Yates Shuffle
+  for(int i = 8; i > 0; i--){
+    randIndex = rand() % (i+1);
+    //swap arr[i] and arr[randIndex]
+    char* temp = arr[i];
+    arr[i] = arr[randIndex];
+    arr[randIndex] = temp;    
+  }
+  gamePlay(arr);
 }
 
-void gamePlay(char* randomizedArray[SIZE]){
+/* gamePlay prints out words, checks the user input,
+   then reprints the word if incorrect input is typed. 
+   it also contains a timer for how long the user takes to win.
+ */
+void gamePlay(char* arr[SIZE]){
   char input[64];
-  //start timer here
+  char c;
+
+  //set timer
+  struct timeval begin;
+  struct timeval finish;
+  struct timeval elapsed;
+  gettimeofday(&begin, NULL); //store time of day in begin
+  
   for(int i = 0; i < SIZE; i++){
-    while(strncmp(input, randomizedArray[i], SIZE) != 0){
-      printf("Word #%d is: %c", i, randomizedArray[i]);
-      scanf("%c", input);
+    while(strncmp(input, arr[i], 10) != 0){ //10 is a max word length
+      printf("Word #%d is '%s': ", i+1, arr[i]);
+      scanf("%s", input);
+      while((c = getchar()) != '\n' && c != EOF) //flush the rest of the line
+	;
     }
   }
   //end timer here
-  
+  gettimeofday(&finish, NULL); //store time of day in finish
+  timersub(&finish, &begin, &elapsed); //timersub(a - b into c)
+  printf("Nice work! You took: %ld seconds and %d microseconds\n", elapsed.tv_sec, elapsed.tv_usec);
 }
